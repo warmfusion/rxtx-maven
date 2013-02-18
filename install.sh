2fb7e5afb37ca3704aab4b7361f087d524bce05e
Toby Jackson <toby@tobyjackson.net>
2013-02-18 20:02:45 +0000

    Adding installs for the i686 platform and changing classifier to be the architecture rather than simply bin

--- install.sh
+++ install.sh
@@ -15,8 +15,15 @@ unzip $NAME-bins.zip > /dev/null
 
 echo Installing Maven artifact: jar
 mvn install:install-file -Dfile=$NAME-bins/RXTXcomm.jar -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=jar -DgeneratePom=true > /dev/null
-echo Installing Maven artifact: bin
-mvn install:install-file -Dfile=$NAME-bins/x86_64-unknown-linux-gnu/librxtxSerial.so -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=so -Dclassifier=bin > /dev/null
+
+
+# Push the binaries for both i686 and x86_64
+echo Installing Maven binary: x86_64-unknown-linux-gnu
+mvn install:install-file -Dfile=$NAME-bins/x86_64-unknown-linux-gnu/librxtxSerial.so -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=so -Dclassifier=x86_64-unknown-linux-gnu > /dev/null
+
+echo Installing serial component for i686-pc-linux-gnu 
+mvn install:install-file -Dfile=$NAME-bins/i686-pc-linux-gnu/librxtxSerial.so -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dpackaging=so -Dclassifier=i686-pc-linux-gnu > /dev/null
+
 
 echo Downloading sources of $NAME
 wget -q http://rxtx.qbang.org/pub/rxtx/$NAME.zip
